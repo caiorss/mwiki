b75f8df87990870dcad813ec6f728511a24dd66e
CaioRS <caiorss.rodrigues@gmail.com>
2024-10-09 09:44:47 -0300

    Add options to bottles' run function: debug and auto reload.

--- mdwiki/__main__.py
+++ mdwiki/__main__.py
@@ -2,4 +2,4 @@ from mdwiki.server import *
 
 if __name__ == '__main__':
     print(" [TRACE] Server started Ok.")
-    run(host='0.0.0.0', port=8060, debug=True)
+    run(host='0.0.0.0', port=8060, debug=True, reloader=True)
