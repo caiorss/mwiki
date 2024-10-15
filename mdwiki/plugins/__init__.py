8e643d25ba2c99c14ea6d9ee227324a026ef9a36
CaioRS <caiorss.rodrigues@gmail.com>
2024-10-15 14:39:34 -0300

    Feature - implement Obsididian markdown  syntax for text highlight.

--- mdwiki/plugins/__init__.py
+++ mdwiki/plugins/__init__.py
@@ -1,4 +1,8 @@
 from ._wiki_link_plugin import wiki_link_plugin
 from ._wiki_image import wiki_image_plugin
+from ._wiki_text_highlight_plugin import wiki_text_highlight_plugin
 
-__all__ = ( "wiki_link_plugin", "wiki_image_plugin" )
+__all__ = (  "wiki_link_plugin"
+           , "wiki_image_plugin"
+           , "wiki_text_highlight_plugin" 
+          )
