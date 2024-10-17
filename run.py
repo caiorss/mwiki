9b329cfadf41fd208fad6692b995470d6b1eb427
CaioRS <caiorss.rodrigues@gmail.com>
2024-10-17 18:41:49 -0300

    Create debug-helper script run.py.

---
+++ run.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python 
+"""Script for starting the server without any command line arguments. 
+
+This file is a loader script for starting the server without any command
+line argument in order to make it easier to run the VSCode debugger
+or any other GUI/IDE based debugger.
+
+Abbreviations:
+ 
+   + GUI - Graphical User Interface 
+   + IDE - Integrated Development Environment
+
+"""
+
+import mdwiki.server 
+
+
+HOST = "0.0.0.0"
+PORT = 8000
+
+mdwiki.server.run_app_server(  host = HOST
+                             , port = PORT
+                             , debug = True
+                             , login = None
+                             , wikipath = "pages"
+                             , random_ssl = False 
+                             )
