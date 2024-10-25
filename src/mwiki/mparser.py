e768e8e33a01025fcc9f242b5c21b55975ab56f3
CaioRS <caiorss.rodrigues@gmail.com>
2024-10-25 00:49:03 -0300

    Feature - Implement LaTeX pseudocode code blocks using pseudocode.js library.
    Now, the following syntax is implemented for
    creating LateX pseudocode.
    
    ```pseudo
     <<pseudo-code-here>>
    ```
    or
    
    ```{pseudo}
     <<pseudo-code-here>>
    ```
    
    Based on:
    + https://github.com/SaswatPadhi/pseudocode.js
    
    And
    
    + https://github.com/ytliu74/obsidian-pseudocode

--- src/mwiki/mparser.py
+++ src/mwiki/mparser.py
@@ -545,6 +545,12 @@ def node_to_html(node: SyntaxTreeNode):
             content, directives = get_code_block_directives(node.content)
             label = f'id="{u}"' if (u := directives.get("label")) else ""
             html = f"""<blockquote {label} >\n{utils.escape_html(content)}\n</blockquote>"""
+        # Compatible with Obsidian's pseudo-code plugin
+        elif info == "pseudo" or info == "{pseudo}":
+            content, directives = get_code_block_directives(node.content)
+            label = f'id="{u}"' if (u := directives.get("label")) else ""
+            #content_ = utils.escape_html(content)
+            html = f"""<pre {label} class="pseudocode" >\n{content}\n</pre>\n"""
         else:
             code = utils.highlight_code(node.content, language = info)
             html = f"""<pre>\n<code class="language-{info.strip()}">{code}</code>\n</pre>"""
