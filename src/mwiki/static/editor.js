b9fd6867a92cb5dfbe9d2c587a00ecd13c700128
CaioRS <caiorss.rodrigues@gmail.com>
2025-10-12 03:56:38 -0300

    Feature - implement file uploading by dragging and dropping.
    Implement file upload by dragging and dropping files from the desktop
    to the Wiki editor. When a file is dragged and dropped on the editor,
    the upload popup window is shown and the user can click at the button
    upload, change the file name or cancel the operation.

--- src/mwiki/static/editor.js
+++ src/mwiki/static/editor.js
@@ -599,33 +599,29 @@ form.addEventListener('submit', handleUploadFormSubmit);
 
 
 function dropHandler(ev) {
-  console.log("File(s) dropped");
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+    console.log("File(s) dropped");
 
-  // Prevent default behavior (Prevent file from being opened)
-  ev.preventDefault();
-
-  if (ev.dataTransfer.items) {
-    // Use DataTransferItemList interface to access the file(s)
-    [...ev.dataTransfer.items].forEach((item, i) => {
-      // If dropped items aren't files, reject them
-      if (item.kind === "file") {
-        const file = item.getAsFile();
-        console.log(`… file[${i}].name = ${file.name}`);
-      }
-    });
-  } else {
-    // Use DataTransfer interface to access the file(s)
-    [...ev.dataTransfer.files].forEach((file, i) => {
-      console.log(`… file[${i}].name = ${file.name}`);
-    });
-  }
+    let file = (ev.dataTransfer.items)
+                ? ev.dataTransfer.items[0].getAsFile() : ev.dataTransfer.files[0].getAsFile();
+    let dt = new DataTransfer();
+    //console.log(" dt = ", dt);
+    dt.items.add(file);
+    let fileInput = document.querySelector("#fileInput");
+    if(!fileInput){
+        // If this message is shown, it means that there is a bug.
+        alert(`Error file input #fileInput not found.`)
+        return;
+    }
+    fileInput.files = dt.files;
+    uploadWindow.show();
 }
 
+// Prevent default behavior (Prevent file from being opened)
 function dragOverHandler(ev) {
-  console.log("File(s) in drop zone");
-
-  // Prevent default behavior (Prevent file from being opened)
   ev.preventDefault();
+  //console.log("File(s) in drop zone");
 }
 
 
